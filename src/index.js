14d45eb10913b4281694590dafb8161035f21a8c
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-04-22 22:10:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-import "./index.css";
 
-class MyLibrary {
-  constructor() {
-    console.log("Library constructor loaded");
-  }
-
-  myMethod = () => {
-    console.log("Library method fired");
-  };
-}
-
-export default MyLibrary;
+export default {};
