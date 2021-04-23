275ef3b3ee699723f37d6d9079666c7021b35b82
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-04-23 15:07:05 +0530

    Delete src/util directory

--- src/util/getClasses.js
+++
@@ -1,9 +0,0 @@
-moonHare.getClasses = function() {
-    var classList = [];
-
-    [].slice.call(window.document.querySelectorAll('*')).forEach(function(el) {
-        [].push.apply(classList, el.classList);
-    });
-
-    return classList;
-}
