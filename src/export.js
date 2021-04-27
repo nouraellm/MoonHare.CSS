659a094747389b077a97efa41ce17cc73e67ef3e
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-04-27 20:24:01 +0530

    Update export.js

--- src/export.js
+++ src/export.js
@@ -1,6 +1,1 @@
-// Export it for webpack
-if (typeof module === 'object' && module.exports) {
-  // Avoid breaking it for `import { u } from ...`. Add `import u from ...`
-  module.exports = mh;
-  module.exports.mh = mh;
-}
+export default moonHare;
