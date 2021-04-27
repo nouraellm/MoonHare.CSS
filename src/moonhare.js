69520510d789907d1af4772af9cea0b0250907aa
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-04-27 20:31:37 +0530

    Update moonhare.js

--- src/moonhare.js
+++ src/moonhare.js
@@ -1,7 +1,32 @@
 var moonHare {};
 
-moonHare.config = {
+moonHare.deepExtend = function(out) {
+  out = out || {};
+
+  for (var i = 1; i < arguments.length; i++) {
+    var obj = arguments[i];
+
+    if (!obj)
+      continue;
+
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (typeof obj[key] === 'object'){
+          if(obj[key] instanceof Array == true)
+            out[key] = obj[key].slice(0);
+          else
+            out[key] = this.deepExtend(out[key], obj[key]);
+        }
+        else
+          out[key] = obj[key];
+      }
+    }
+  }
+
+  return out;
 };
+
+moonHare.config = {};
 
 moonHare.theme = {
     screens: {
